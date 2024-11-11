62bbc02e57f2f040611d2ad4305612710ee693a2
rmz01 <alu0101438238@ull.edu.es>
2024-11-11 13:15:27 +0000

    probando bleed

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Mi practica</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://www.ull.es/',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/rmz01/nextra-example',
   footer: {
     text: 'Nextra Docs Template',
   },
